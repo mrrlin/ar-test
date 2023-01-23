b81c5d25ae1690ca85b36706dd5248c58d17e01e
Angelina <bunny.iblis@gmail.com>
2023-01-23 22:14:06 +0300

    Image tracking (avoid camera restriction)

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-import * as THREE from '../libs/three.js-r132/build/three.module.js';
+const THREE = window.MINDAR.IMAGE.THREE;
+import {mockWithVideo, mockWithImage} from '../libs/camera-mock.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-    const scene = new THREE.Scene();
+document.addEventListener('DOMContentLoaded', () => {
+    const start = async () => {
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
-    const cube = new THREE.Mesh(geometry, material);
+        //mockWithVideo("../assets/mock-videos/banner1.mp4");
+        mockWithImage("../assets/mock-videos/banner.png");
 
-    scene.add(cube);
-    cube.position.set(0, 0, -2);
-    cube.rotation.set(0, Math.PI/4, 0);
+        // navigator.mediaDevices.getUserMedia = () => {
+        //     return new Promise((resolve, reject) => {
+        //         const video = document.createElement("video");
+        //         video.setAttribute("src", "../assets/mock-videos/banner1.mp4");
+        //         video.setAttribute("loop", "");
 
-    const camera = new THREE.PerspectiveCamera();
-    camera.position.set(1, 1, 5);
+        //         video.oncanplay = () => {
+        //             video.play();
+        //             resolve(video.captureStream());
+        //         }
+        //     });
+        // }
 
-    const renderer = new THREE.WebGLRenderer({alpha: true});
-    renderer.setSize(500, 500);
-    renderer.render(scene, camera);
-
-    const video = document.createElement("video");
-    navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
-        video.srcObject = stream;
-        video.play();
-    });
-
-    video.style.position = "absolute";
-    video.style.width = renderer.domElement.width;
-    video.style.height = renderer.domElement.height;
-    renderer.domElement.style.position = "absolute";
-
-    document.body.appendChild(video);
-    document.body.appendChild(renderer.domElement);
+        const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+            container: document.body,
+            imageTargetSrc: '../assets/targets/banner.mind'
+        });
+        const {renderer, scene, camera} = mindarThree;
+    
+        const geometry = new THREE.PlaneGeometry(1, 1);
+        const material = new THREE.MeshBasicMaterial({color: 0x0000ff, transparent: true, opacity: 0.5 });
+        const plane = new THREE.Mesh(geometry, material);
+    
+        const anchor = mindarThree.addAnchor(0);
+        anchor.group.add(plane); // THREE.Group
+    
+        await mindarThree.start();
+        renderer.setAnimationLoop(() => {
+            renderer.render(scene, camera);
+        });
+    }
+    start();
 });
